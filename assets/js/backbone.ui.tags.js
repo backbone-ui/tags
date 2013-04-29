62426063ffb8c4cd61fd1e247b1c7f81323914dc
Makis Tracend <makis.tracend@gmail.com>
2013-04-28 19:55:43 -0700

    introducing internal collection/model to parse the tags from an array

--- assets/js/backbone.ui.tags.js
+++ assets/js/backbone.ui.tags.js
@@ -13,7 +13,7 @@
 	// Support backbone app (if available)
 	var View = ( typeof APP != "undefined" && !_.isUndefined( APP.View) ) ? APP.View : Backbone.View;
     
-	// APP editing
+	// Views
 	Backbone.UI.Tags = View.extend({
         
         options: {
@@ -26,37 +26,29 @@
 		},
         
 		initialize: function( options ){
-			//fallbacks
-			options || (options = {});
+            // 
 			_.bindAll( this, 'render', 'addTag', 'delTag', 'newTag', 'cleanTag', 'updateField');
-			//
-			this.$field = options.field || false;
-			// put these in a template?
-			this.views = {
-				"tag" : '<li class="tag"><a><span class="label"></span><span class="del">×</span></a></li>'
-			};
-			
-			this.render();
-			//return APP.View.prototype.initialize.apply( this, options );
+			// check if the data passed is a "static" array
+            if( options.data instanceof Array ){
+                this.data = new Tags( options.data );
+            }
+            
+            return View.prototype.initialize.call( this, options );
 		}, 
         
-		render: function(){
-			if( !this.$field ) return;
-			// get the tags from the input field
-			var field = this.$field.val() || false;
-			if(!field || _.isEmpty(field)) return;
-			//
-			var tags = (field).split("|");
-			// reverse the order so they are added in the right order :P
-			tags.reverse();
-			for(var i in tags){
-				this.addTag( tags[i] );
-			}
-		}, 
+        render: function(){
+            
+            console.log( this.data.toJSON() );
+			
+        },
         
 		addTag: function( label ){
-			var template = this.views.tag;
-			$(template).find(".label").html( label ).closest(".tag").prependTo(this.el);
+			//var template = this.views.tag;
+            if( this.data ){ 
+                this.data.add( new Tag( label ) );
+            }
+			//$(template).find(".label").html( label ).closest(".tag").prependTo(this.el);
+            this.render();
 		}, 
         
 		delTag: function( e ){
@@ -105,5 +97,39 @@
 		}
 	});
 	
+    
+    var Tag = Backbone.Model.extend({
+        
+    });
+    
+    var Tags = Backbone.Collection.extend({
+        
+        model: Tag,
+        
+        toJSON: function(){
+            return _.map( this.models, function( tag ) {
+                // return the first (an only) key from each tag
+                return tag.keys()[0];
+            });
+        },
+        
+		parse: function(){
+            console.log( this.options.template );
+			//if( !this.$field ) return;
+			// get the tags from the input field
+            console.log( this.model.toJSON() );
+			//var field = this.$field.val() || false;
+			//if(!field || _.isEmpty(field)) return;
+			//
+			//var tags = (field).split("|");
+			// reverse the order so they are added in the right order :P
+			//tags.reverse();
+			//for(var i in tags){
+			//	this.addTag( tags[i] );
+			//}
+            
+		} 
+        
+    });
 
 })(this._, this.Backbone);
