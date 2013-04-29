6dd18a7343334dfc566272393eb73d79144cea6b
Makis Tracend <makis.tracend@gmail.com>
2013-04-29 01:50:27 -0700

    updating tag model with options

--- assets/js/backbone.ui.tags.js
+++ assets/js/backbone.ui.tags.js
@@ -33,32 +33,23 @@
 			_.bindAll( this, 'render', 'addTag', 'delTag', 'newTag', 'cleanTag', 'updateField');
 			// check if the data passed is a "static" array
             if( options.data instanceof Array ){
-                this.data = new Tags( options.data );
+                this.data = new Tags( options.data, this.options );
             }
             // import data from field 
             if( options.fieldName ){
                 this.parseField();
             }
+            
             return View.prototype.initialize.call( this, options );
 		}, 
-        /*
-        render: function(){
-            
-            console.log( this.data.toJSON() );
-			
-        },
-        */
+        
 		addTag: function( label ){
 			//var template = this.views.tag;
             if( this.data ){ 
-                this.data.add( new Tag( label ) );
+                this.data.add( new Tag({ id: label, editable: this.options.editable }) );
             }
             this.updateField();
-            
-            if( this.options.tagTemplate ){ 
-                var template = this.options.tagTemplate;
-                $(template).find(".label").html( label ).closest(".tag").prependTo(this.el);
-            }
+            this.render();
 		}, 
         
 		delTag: function( e ){
@@ -67,7 +58,12 @@
 			var tag = $(e.target).closest(".tag");
 			// remove tag
 			$(this.el).find(tag).remove();
-			// update input field
+            // update data
+            if( this.data ){ 
+                var id = tag.find(".label").html();
+                this.data.remove( id );
+            }
+            // update input field
 			this.updateField();
 		}, 
         
@@ -80,7 +76,7 @@
 					var tag = this.cleanTag( $(e.target).val() );
 					this.addTag( tag );
 					// reset input
-					$(e.target).val("");
+                    $(e.target).val("");
 					// prevent form submission
 					return false;
 			}
@@ -91,18 +87,17 @@
 			return string.replace(/http:\/\/|https:\/\/|www./gi, "");
 		}, 
         
-        save: function(){
-           // update data
-					this.updateField();
-					 
-        }, 
-        
         parseField: function(){
             this.$field = $(this.options.fieldName);
             var tags = this.$field.val() || false;
 			if(!tags || _.isEmpty(tags)) return;
             var dl = this.options.fieldDelimiter;
-            this.data = new Tags( tags.split( dl ) );
+            // add to the existing data, if available
+            if( this.data ){ 
+                this.data.add( tags.split( dl ) );
+            } else {
+                this.data = new Tags( tags.split( dl ), this.options );
+            }
         }, 
         
 		updateField: function(){
@@ -113,7 +108,8 @@
 			//});
 			// make a string from the tags
             var dl = this.options.fieldDelimiter;
-			var value = this.data.toJSON().join( dl );
+            console.log( this.data.toArray() );
+			var value = this.data.toArray().join( dl );
 			// update input (hidden) field
 			this.$field.val( value );
 			
@@ -122,25 +118,66 @@
 	
     
     var Tag = Backbone.Model.extend({
-        
+        initialize: function( data, options ){
+            // flags
+            if( typeof data == "string" ){
+                data = { id : data };
+            }
+        }
     });
     
     var Tags = Backbone.Collection.extend({
         
         model: Tag,
         
+        options: {
+            
+        }, 
+        
+        initialize: function(models, options){
+            // set options
+            this.options.editable = options.editable || false; 
+        }, 
+        /*
         toJSON: function(){
+            var self = this;
             return _.map( this.models, function( tag ) {
                 // return the first (an only) key from each tag
-                return tag.keys()[0];
+                return { id : tag.keys()[0], editable : self.options.editable };
             });
+            
+        },
+        */
+        
+        // revert back to the array format when needed
+        toArray: function(){
+            var self = this;
+            return _.map( this.models, function( tag ) {
+                // return the first (an only) key from each tag
+                return tag.id;
+            });
+            
         },
         
-		parse: function(){
-            console.log( this.options.template );
+        // form the imported data...
+        add: function( data, options ){
+            if( data instanceof Array ){
+                // it is expected that each item is an object
+                for( var i in data ){
+                   if(typeof data[i] == "string"){
+                       data[i] = new Tag({ id : data[i], editable : this.options.editable });   
+                   }
+                }
+            }
+            return Backbone.Collection.prototype.add.call( this, data, options );
+        },
+        
+        /*
+		parse: function( data ){
+            console.log( this.options );
 			//if( !this.$field ) return;
 			// get the tags from the input field
-            console.log( this.model.toJSON() );
+            //console.log( this.model.toJSON() );
 			//var field = this.$field.val() || false;
 			//if(!field || _.isEmpty(field)) return;
 			//
@@ -150,9 +187,9 @@
 			//for(var i in tags){
 			//	this.addTag( tags[i] );
 			//}
-            
+            console.log( data );
 		} 
-        
+        */
     });
 
 })(this._, this.Backbone);
